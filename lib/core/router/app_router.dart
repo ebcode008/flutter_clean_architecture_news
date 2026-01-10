b3dd3a4df0b575e4339542d1242808fe92da6d39
Eliab Acevedo Avila <eliabdrumera@gmail.com>
2026-01-09 21:55:24 -0500

    commit

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -5,22 +5,22 @@ import 'package:flutter_clean_architecture_01/features/daily_news/presentation/p
 import 'package:go_router/go_router.dart';
 
 class AppRoutes {
-  static const newsArticles = 'newsarticles';
-  static const savedArticles = 'savedarticles';
-  static const articleDetail = 'articledetail';
+  static const newsArticles = 'news-articles';
+  static const savedArticles = 'saved-articles';
+  static const articleDetail = 'article-detail';
 }
 
 final GoRouter router = GoRouter(
-  initialLocation: '/newsarticles',
+  initialLocation: '/news-articles',
   routes: [
-    GoRoute(path: '/newsarticles', name: AppRoutes.newsArticles, builder: (context, state) => const NewsArticlePage()),
+    GoRoute(path: '/news-articles', name: AppRoutes.newsArticles, builder: (context, state) => const NewsArticlePage()),
     GoRoute(
-      path: '/savedarticles',
+      path: '/saved-articles',
       name: AppRoutes.savedArticles,
       builder: (context, state) => const SavedArticlesPage(),
     ),
     GoRoute(
-      path: '/articledetail',
+      path: '/article-detail',
       name: AppRoutes.articleDetail,
       builder: (context, state) {
         final article = state.extra as Article;
